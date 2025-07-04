a3c4d572a474592303f7e5463213d8bf1fda34cb
Jasleen Kaur <jasleen.matharoo@s.amity.edu>
2025-07-05 02:32:11 +0530

    update

--- project/tailwind.config.js
+++ project/tailwind.config.js
@@ -1,5 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
